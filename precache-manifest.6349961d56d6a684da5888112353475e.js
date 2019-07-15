a589f52f5823121ca16a6142900a088845d75bf5
QuintyHH <razmocanu@yahoo.com>
2019-07-15 10:08:55 +0300

    Updates

---
+++ precache-manifest.6349961d56d6a684da5888112353475e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3c5ec929d4b9d3051e6401691e06c0b",
+    "url": "/react-chat-posts-samp/index.html"
+  },
+  {
+    "revision": "546c305b44e94e5c5379",
+    "url": "/react-chat-posts-samp/static/js/2.996a6753.chunk.js"
+  },
+  {
+    "revision": "b3e444d4838211b760ed",
+    "url": "/react-chat-posts-samp/static/js/main.9f3727e5.chunk.js"
+  },
+  {
+    "revision": "1d2b54613973f047e94a",
+    "url": "/react-chat-posts-samp/static/js/runtime~main.28866d7e.js"
+  }
+]);
